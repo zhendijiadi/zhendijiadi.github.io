bf771f3bf779f6087031ca11b77f3f9ce1d6a0b0
Tommy <zhendijiadi@outlook.com>
2024-08-30 10:56:31 +0800

    2024-08-30 10:56:30

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1724937231',resources: [
+    cacheName: 'chirpy-1724986548',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
